a83922d1ac0359446a41fd731f1364a68a74d503
Annapurna Mishra <146976500+Annapurna0311@users.noreply.github.com>
2024-06-15 23:41:42 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,76 +1,60 @@
-import React, { useState } from 'react'
-import Add from '../images/add.png'
-import { auth, storage , db} from '../firebase'
+import React, { useState } from "react";
+import Add from "../images/add.png";
+import { auth, storage, db } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
-const Signup = () =>{
-    
-    const [err,setErr] = useState(false)
+const Signup = () => {
+    const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const createUser = () => {
+    createUserWithEmailAndPassword(auth,email,password).then(value => alert('Success'));
+  }
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">WhatsOn</span>
+        <span className="title">Sign Up</span>
+        <form >
+          <input
+          onChange={e => setUsername(e.target.value)}
+          value={username}
+            type="text"
+            id="name"
+            placeholder="Enter name"
+            fdprocessedid="1bryqm"
+            autoComplete="username"
+          />
+          <input
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            id="email"
+            placeholder="Email"
+            fdprocessedid="1bryqm"
+            autoComplete="email"
+          />
+          <input
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+            type="password"
+            id="password"
+            placeholder="Password"
+            fdprocessedid="1bryqm"
+            autoComplete="current-password"
+          />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add avatar</span>
+          </label>
+          <button onClick={createUser}>Sign Up</button>
+        </form>
+        <p>Do have an account? Login</p>
+      </div>
+    </div>
+  );
+};
 
-    const handleSubmit = async(e) =>{
-        e.preventDefault()
-        const displayName = e.target[0].value;
-        const email = e.target[1].value;
-        const password = e.target[2].value;
-        const file = e.target[3].files[0];
-
-    try{    
-    const res = await createUserWithEmailAndPassword(auth, email, password)
-    const storageRef = ref(storage, displayName);
-
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-    (error) => {
-        setErr(true);
-    }, 
-    () => {
-        
-        getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-        await updateProfile(res.user,{
-            displayName,
-            photoURL:downloadURL,
-        });
-        await setDoc(doc(db,"users", res.user.id),{
-            uid: res.user.uid,
-            displayName,
-            email,
-            photoURL: downloadURL,
-        });
-        });
-    }
-    );
-
-
-    }catch(err){
-        setErr(true);
-    }
-      
-    };
-    return (
-        <div className='formContainer'>
-            <div className='formWrapper'>
-            <span className='logo'>WhatsOn</span>
-            <span className='title'>Sign Up</span>
-                <form onSubmit={handleSubmit}>
-                    <input type='text' placeholder='Enter name'/>
-                    <input type='email' placeholder='Email'/>
-                    <input type='password' placeholder='Password'/>
-                    <input style={{display:'none'}}type='file' id='file'/>
-                    <label htmlFor='file'>
-                        <img src={Add} alt=''/>
-                        <span>Add avatar</span>
-                    </label>
-                    <button>Sign Up</button>
-                    {err && <span>Something went wrong</span>}
-                </form>
-                <p>Do have an account? Login</p>
-            </div>
-        </div>
-
-    )
-}
-
-export default Signup
-
+export default Signup;
