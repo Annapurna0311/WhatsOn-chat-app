ee4593cc7262230bcd220f0d8e7da63ccf93c653
Annapurna Mishra <146976500+Annapurna0311@users.noreply.github.com>
2024-06-15 23:43:17 +0530

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,10 +4,10 @@ import Attach from '../images/attachment.png'
 const Input = () => {
   return (
     <div className='input'>
-      <input type='text' placeholder='Message'/>
+      <input type='text' id="text-input" placeholder='Message'/>
       <div className='send'>
         <img src={Attach}  alt=""/>
-        <input type='file' style={{display:'none'}} id='file'/>
+        <input type='file' style={{display:'none'}} id='file-attach-chat'/>
         <label htmlFor='file'>
             <img src={Img} alt=''/>
         </label>
