d926138241da46f85435a901b7f2638f85a46765
Annapurna Mishra <146976500+Annapurna0311@users.noreply.github.com>
2024-06-15 23:43:50 +0530

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,7 +4,7 @@ const Searchbar = () => {
   return (
     <div className='search'>
       <div className='searchForm'>
-        <input type='text' placeholder='Search'/>
+        <input type='text' id="text-search" placeholder='Search'/>
       </div>
       <div className='userChat'>
          <img src='https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg' alt=''/>
