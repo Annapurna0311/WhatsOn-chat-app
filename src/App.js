cb1c1784517c380af5da23029ba57d28e4fd1cf6
Annapurna Mishra <146976500+Annapurna0311@users.noreply.github.com>
2024-07-27 01:06:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Signup from './pages/Signup'
-import './style.scss'
 import Login from './pages/Login'
+import './style.scss'
 import Home from './pages/Home'
+import Layout from './pages/Layout'
+import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom";
+import { AuthContext } from './context/AuthContext'
 function App() {
+  const {currentUser} = useContext(AuthContext)
+  const ProtectedRoute = ({children})=>{
+    if(!currentUser){
+      return <Navigate to="/login"/>
+    }
+  }
   return (
-    <div>
-      <Signup/>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<ProtectedRoute>
+            <Home/>
+          </ProtectedRoute>} />
+          <Route path="login" element={<Login/>} />
+          <Route path="signup" element={<Signup/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
